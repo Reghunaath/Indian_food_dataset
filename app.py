9211a445a4668dfe4d0b6c2dda22b43c1869d387
Reghunaath <56317070+Reghunaath@users.noreply.github.com>
2022-01-08 18:48:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,request,jsonify
+import requests as r
+import json
+app=Flask(__name__)
+@app.route('/api',methods=['GET'])
+def start():
+    url = 'http://192.168.0.151:8090/json-rpc'
+    myobj = {
+      "command": "color",
+      "color": json.loads(request.args['c']),
+      "priority": 50,
+      "origin": "My Fancy App"
+    }
+    x = r.post(url,  json=myobj,headers = {"Authorization": "token 46a2032e-da1b-4c20-b690-27413aa43589"})
+    #print(x)
+    return x.json()
+if __name__ == '__main__':
+    app.run()
